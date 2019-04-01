d249f708ab940d192f2bd5685ed0078994fe05d3
Kuba <ja.tanski@gmail.com>
2019-04-01 14:01:56 +0200

    fix bug in config

--- babel.config.js
+++ babel.config.js
@@ -1,9 +1,9 @@
-const presents = [
+const presets = [
     [
         "@babel/env",
         {
             targets: {
-                ende: "17",
+                edge: "17",
                 firefox: "60",
                 chrome: "67",
                 safari: "11.1",
@@ -15,5 +15,5 @@ const presents = [
 ];
 
 module.exports = {
-    presents
+    presets
 }
