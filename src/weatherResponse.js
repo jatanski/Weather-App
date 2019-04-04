d2c64cb6dbe63647984eb4829edf3f6d7cb036dc
Mateusz Pichniarczyk <mat.pich@interia.pl>
2019-04-04 21:07:04 +0200

    minor changes

--- src/weatherResponse.js
+++ src/weatherResponse.js
@@ -3,9 +3,23 @@ const apiKey = '4c35b4e8b88542c58826712e428d23ff';
 
 export const curWeather = async (param) => {
     const url = `https://api.weatherbit.io/v2.0/current?${param}&key=${apiKey}`;
-
     const fetching = await fetch(url);
-    return await fetching.json();
+    const currentWeather = await fetching.json();
+    return currentWeather.data[0];
+}
+
+export const hourWeather = async (param) => {
+    const url = `https://api.weatherbit.io/v2.0/forecast/hourly?${param}&key=${apiKey}&hours=5`;
+    const fetching = await fetch(url);
+    const weatherHourArray = await fetching.json();
+    return weatherHourArray.data;
+}
+
+export const daysWeather = async (param) => {
+    const url = `https://api.weatherbit.io/v2.0/forecast/daily?${param}&key=${apiKey}&days=5`;
+    const fetching = await fetch(url);
+    const weatherDaysArray = await fetching.json();
+    return weatherDaysArray.data;
 }
 
 // export const curWeather = {
