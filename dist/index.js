7c80128bbb76d116592475ae7c25827a716a069e
Kuba <ja.tanski@gmail.com>
2019-04-10 16:31:33 +0200

    change api key

--- dist/index.js
+++ dist/index.js
@@ -6917,7 +6917,7 @@ function asyncGeneratorStep(gen, resolve, reject, _next, _throw, key, arg) { try
 
 function _asyncToGenerator(fn) { return function () { var self = this, args = arguments; return new Promise(function (resolve, reject) { var gen = fn.apply(self, args); function _next(value) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "next", value); } function _throw(err) { asyncGeneratorStep(gen, resolve, reject, _next, _throw, "throw", err); } _next(undefined); }); }; }
 
-var apiKey = '4c35b4e8b88542c58826712e428d23ff';
+var apiKey = '414ab9a5d20043a8a7055ff0b31dbbaf';
 var curWeather =
 /*#__PURE__*/
 function () {
